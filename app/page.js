69d9e1c7e53c398579ce8f0de773fe7e9b3163ff
Festus <techarenaz01@gmail.com>
2025-03-04 00:37:25 -0800

    added text color to test tailwind css

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
+          <li className="mb-2 text-green-500">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.js
