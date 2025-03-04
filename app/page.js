acfdd6e5f1b86f0cbf64669d90d662e6124aa142
Festus <techarenaz01@gmail.com>
2025-03-04 01:13:29 -0800

    added components folder and cleaned the code in app layout

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center mt-96 text-4xl underline uppercase text-purple-600">Tech Arena24 Blog</h1>
+      <h1 className="text-center mt-96 text-4xl uppercase underline">Tech Arena24 Blog</h1>
     </div>
   );
 }
