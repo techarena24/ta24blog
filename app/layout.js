ad03710026f73d3d8edf75cce3d189d7e8b6a135
Festus <techarenaz01@gmail.com>
2025-03-07 14:02:30 -0800

    added max-w with border color blue

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-6xl mx-auto border-2 border-blue-400`}
       >
         {children}
       </body>
