f4d1823d62298433c8cbe703dad0dbb6aa07b2d2
Festus <techarenaz01@gmail.com>
2025-03-08 00:47:48 -0800

    testing with pull and status on git with Sir Will

--- app/(client)/page.js
+++ app/(client)/page.js
@@ -10,6 +10,7 @@ export default function Home() {
       <h4 className=" text-4xl">24/7 hOURS</h4>
       <p className=" text-red-600 text-5xl">Log Out</p>
       <h2 className=" text-blue-800 text-7xl">WELCOME</h2>
+      <h2 className=" text-blue-800 text-2xl">Another WELCOME Text</h2>
     </div>
   );
 }
