faf05a37b9be591e9e96306bb44d55a6a6d38986
Festus <techarenaz01@gmail.com>
2025-03-07 23:45:58 -0800

    just something

--- app/(client)/page.js
+++ app/(client)/page.js
@@ -4,7 +4,9 @@ export default function Home() {
       <h1 className="text-center my-28 text-4xl uppercase underline text-red-900">
         Tech Arena24 Blog
       </h1>
-      <h2 className="text-green uppercase font-bold">Every Day Tech News for Users</h2>
+      <h2 className="text-green uppercase font-bold">
+        Every Day Tech News for Users
+      </h2>
       <h4 className=" text-4xl">24/7 hOURS</h4>
       <p className=" text-red-600 text-5xl">Log Out</p>
     </div>
