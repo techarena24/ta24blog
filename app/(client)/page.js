182587be98417640e5a7b904728a6786966d5443
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2025-03-08 07:44:23 +0000

    commit

--- app/(client)/page.js
+++ app/(client)/page.js
@@ -7,6 +7,7 @@ export default function Home() {
       <h2 className="text-green uppercase font-bold">Every Day Tech News for Users</h2>
       <h4 className=" text-4xl">24/7 hOURS</h4>
       <p className=" text-red-600 text-5xl">Log Out</p>
+      <h2 className=" text-blue-800 text-7xl">WELCOME</h2>
     </div>
   );
 }
