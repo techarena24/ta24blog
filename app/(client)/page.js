ed9997fc59b22d23c8d5790cd83c0d3f97b384b7
Festus <techarenaz01@gmail.com>
2025-03-07 23:28:38 -0800

    changed the top and bottom margin for text

--- app/(client)/page.js
+++ app/(client)/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center mt-96 text-4xl uppercase underline text-red-900">
+      <h1 className="text-center my-28 text-4xl uppercase underline text-red-900">
         Tech Arena24 Blog
       </h1>
       <h2 className="text-green uppercase font-bold">Every Day Tech News for Users</h2>
