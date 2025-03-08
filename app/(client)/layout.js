390c88efaf2d1fdf858304380154fd5936f33531
Festus <techarenaz01@gmail.com>
2025-03-07 19:25:05 -0800

    added a demo header for the page.

--- app/(client)/layout.js
+++ app/(client)/layout.js
@@ -22,6 +22,16 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-6xl mx-auto border-3 border-blue-400`}
       >
+        <header className="flex gap-2 w-[95%] mx-auto py-3 border-b-2 border-blue-600">
+          <nav className="bg-amber-600 w-[20%]">Logo</nav>
+          <div className="flex justify-between bg-amber-300 w-[60%]">
+            <a href="/about">About</a>
+            <a href="/about">About</a>
+            <a href="/about">About</a>
+            <a href="/about">About</a>
+          </div>
+          <div className="bg-amber-900 w-[20%]">Search</div>
+        </header>
         {children}
       </body>
     </html>
