1a1d08d97c017fae72c6d242af71c386d965cda5
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2025-03-09 14:13:25 +0000

    header navbar is added

---
+++ app/(client)/components/Navbar.jsx
@@ -0,0 +1,54 @@
+'use client';
+
+import Image from 'next/image'
+import React from 'react'
+import logo from "../../../public/images/logoTa24.jpeg"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const links = [
+    { id: 1, name: "News", href: "/news" },
+    { id: 2, name: "Reviews", href: "/reviews" },
+    { id: 3, name: "Deals", href: "/deals" },
+    { id: 4, name: "Latest Devices", href: "/latest" },
+    { id: 5, name: "Contact", href: "/contact" },
+    { id: 6, name: "About Us", href: "/about" },
+]
+
+const Navbar = () => {
+    const pathName = usePathname();
+
+  return (
+    <header className=' mb-8 border-b border-b-primary '>
+        <div className=' max-w-6xl mx-auto flex justify-between items-center h-18'>
+            <Link href={"/"}>
+                <Image 
+                    src={logo}
+                    alt="Tech Arena24 logo"
+                    width={180}
+                    height={100}
+                    priority
+                />
+            </Link>
+
+            <nav className=' flex gap-10'>
+                {links.map((link, id) => (
+                    <div key={id}>
+                        {pathName === link.href ? (
+                            <Link className=' text-primary font-semibold' href={link.href}>
+                                {link.name}
+                            </Link>
+                        ) : (
+                            <Link className=' font-semibold transition duration-100 hover:text-primary' href={link.href}>
+                                {link.name}
+                            </Link>
+                        )}
+                    </div>
+                ))}
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
--- app/(client)/layout.js
+++ app/(client)/layout.js
@@ -1,5 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +12,12 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "900"],
+  variable: "--font-poppins",
+})
+
 export const metadata = {
   title: "Tech Arena24",
   description: "Your Everyday Technology News",
@@ -18,20 +25,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-6xl mx-auto border-3 border-blue-400`}
-      >
-        <header className="flex gap-2 max-w-5xl mx-auto py-3 border-b-2 border-blue-600">
-          <nav className="bg-amber-600 w-[20%]">Logo</nav>
-          <div className="flex justify-between bg-amber-300 w-[60%]">
-            <a href="/about">About</a>
-            <a href="/about">About</a>
-            <a href="/about">About</a>
-            <a href="/about">About</a>
-          </div>
-          <div className="bg-amber-900 w-[20%]">Search</div>
-        </header>
+    <html lang="en" className={poppins.className}>
+      <body className={` antialiased `}>
+        <Navbar />
         {children}
       </body>
     </html>
