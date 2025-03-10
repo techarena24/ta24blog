a1ee8aa4e60758f4a55de60aa707b0783ad3fe53
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2025-03-10 22:08:43 +0000

    commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 import React from 'react'
-import logo from "../../../public/images/logoTa24.jpeg"
+import logo from "../../public/images/logoTa24.jpeg"
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { ModeToggle } from './ModeToggle';
