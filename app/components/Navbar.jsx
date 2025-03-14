04e0675c35c191bfc3a6c19579596213de5a266c
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2025-03-14 14:58:12 +0000

    padding is added to the whole website

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,13 +1,13 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../public/images/logoTa24.jpeg";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ModeToggle } from "./ModeToggle";
 import { Button } from "@/components/ui/button";
-import { MenuIcon, SearchIcon } from "lucide-react";
+import { MenuIcon, SearchIcon, X } from "lucide-react";
 
 const links = [
   { id: 1, name: "News", href: "/news" },
@@ -20,10 +20,11 @@ const links = [
 
 const Navbar = () => {
   const pathName = usePathname();
+  const [menuOpen, setMenuOpen] = useState(false)
 
   return (
     <header className="max-w-6xl mx-auto mb-8">
-      <div className="flex justify-between items-center h-18 border-b border-b-primary">
+      <div className="flex justify-between items-center h-18 border-b border-b-primary px-6 sm:px-4 lg:px-0">
         <Link href={"/"}>
           <Image
             src={logo}
@@ -34,6 +35,7 @@ const Navbar = () => {
           />
         </Link>
 
+        {/* Desktop Navigation */}
         <nav className=" hidden lg:flex gap-10">
           {links.map((link, id) => (
             <div key={id}>
@@ -53,16 +55,40 @@ const Navbar = () => {
           ))}
         </nav>
         
+        {/* Buttons */}
         <div className=" flex flex-row gap-5 ">
           <ModeToggle />
-          <Button className=" lg:hidden">
-             <MenuIcon />
+          <Button className=" lg:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? <X /> : <MenuIcon />}
           </Button>
         </div> 
       </div>
+      
+      {/* Mobile Navigation */}
+      <div className={`bg-blue-950 fixed top-0 left-0 h-screen w-full z-50 transform ${menuOpen ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"} 
+      transition-transform duration-300 ease-in-out lg:hidden`}>
+          <div className=" flex justify-between items-center p-5">
+            <h2 className=" text-xl font-semibold">
+              Menu
+            </h2>
+            <Button onClick={() => setMenuOpen(false)}>
+              <X />
+            </Button>
+          </div>
 
-      <div className=" bg-primary">
-        <div className=" max-w-[50%] mx-auto h-12 flex items-center">
+          <nav className=" flex flex-col items-center space-y-6 mt-10">
+            {links.map((link, id) => (
+              <Link key={id} href={link.href} className=" text-white text-lg font-semibold hover:text-primary" 
+              onClick={() => setTimeout(() => setMenuOpen(false), 100)}>
+                {link.name}
+              </Link>
+            ))}
+          </nav>
+      </div>
+      
+      {/* Search Bar */}
+      <div className=" bg-primary px-6 sm:px-4">
+        <div className=" md:max-w-[50%] mx-auto h-12 flex items-center">
           <div className=" flex w-full rounded-tr-sm rounded-br-sm border dark:border-secondary ">
             <input 
               type="text" 
