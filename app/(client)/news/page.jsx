b4a75c0dd9a6b009495d2b2d6d57335e8f96ea5b
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2025-03-30 22:49:39 +0100

    commit

--- app/(client)/news/page.jsx
+++ app/(client)/news/page.jsx
@@ -1,8 +1,30 @@
+import { posts } from '@/app/components/LatestPosts'
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Welcome to news page</div>
+    <div className=' flex gap-8 items-start'>
+      <div className=' flex-[70%] border'>
+        <div>
+          {posts.length > 0 && (
+            <div key={posts[0].id} className=''>
+              <Image 
+                src={posts[0].img}
+                alt={posts[0].title}
+                width={500}
+                height={300}
+                priority
+                className=' w-full h-80 object-contain'
+              />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className=' flex-[30%] border h-screen'>
+        <p>ads</p>
+      </div>
+    </div>
   )
 }
 
