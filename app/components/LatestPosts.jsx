28019596e98b105040cee6107c582eafb1d51257
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2025-04-26 00:42:16 +0100

    query for all posts in the homepage was fetched from sanity

--- app/components/LatestPosts.jsx
+++ app/components/LatestPosts.jsx
@@ -1,9 +1,12 @@
-
 import { Button } from '@/components/ui/button'
 import { slugify } from '@/lib/slugify'
+import { client } from '@/sanity/lib/client'
+import { formatDistanceToNow } from 'date-fns'
+import { toPlainText } from 'next-sanity'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+
 
 
 export const posts = [
@@ -108,32 +111,72 @@ export const posts = [
     date: "10 mins ago",
   },
 ]
-const LatestPosts = () => {
+
+
+
+/*{ fetching all posts from sanity backend }*/
+const getAllPosts = async () => {
+  const query = `*[_type == 'post'] {
+    _id,
+    title,
+    slug,
+    "postImage": postImage.asset->url,
+    "author": author->name,
+    publishedAt,
+    "categories": categories[]->{title, slug},
+    body,
+  }`
+
+  const posts = await client.fetch(query);
+  return posts;
+}
+
+const LatestPosts = async () => {
+  const data = await getAllPosts()
+
+  const truncatedText = (text, length) => {
+    return text.length > length ? text.slice(0, length) + '...' : text;
+  };
 
   return (
     <div className=' flex flex-col flex-[2] space-y-6'>
         <h1 className=' text-2xl lg:text-3xl font-semibold'>Latest Posts</h1>
         <div>
             <div className=' grid sm:grid-cols-2 gap-6'>
-                {posts.map((post) => (
-                  <Link key={post.id} href={`/${post.slug}`}>
-                    <div className=' flex flex-row p-4 gap-4 sm:flex-col border border-gray-300 shadow-sm rounded-sm overflow-hidden'>
+                {data.map((post) => ( 
+                  <Link key={post._id} href={`/${post.slug?.current}`} aria-label={`Read more about ${post.title}`}>
+                    <div className=' flex flex-row gap-4 p-1 sm:flex-col overflow-hidden'>
                       <Image 
-                        src={post.img} 
-                        alt={post.title}
+                        src={post.postImage} 
+                        alt={`Image for the post titled ${post.title}`}
                         width={100} 
                         height={80} 
                         priority
-                        className=' w-32 h-28 rounded-sm bg-white object-contain sm:mt-0 sm:ml-0 sm:w-full sm:h-[200px] sm:rounded-t-sm '
+                        className=' w-32 h-28 bg-white object-fill sm:mt-0 sm:ml-0 sm:w-full sm:h-[200px]'
                       />
                       <div className=' space-y-2'>
                         <div className=' hidden sm:flex text-xs text-gray-600 justify-between'>
-                          <h4 className=' font-semibold text-primary'>{post.category}</h4>
+                          <h4 className=' font-semibold text-primary'>
+                            {post.categories.map((category) => (
+                              <span key={category.slug.current} className="inline-block mr-2 last:mr-0">
+                                {category.title}
+                              </span>
+                            ))}
+                          </h4>
                           <h4>{post.author}</h4>
                         </div>
-                        <h2 className=' text-base sm:text-lg font-bold leading-normal hover:text-primary line-clamp-3'>{post.title}</h2>
-                        <p className=' hidden sm:block text-sm text-gray-500 line-clamp-3'>{post.content}</p>
-                        <p className=' text-xs text-gray-400'>{post.date}</p>
+                        <h2 className=' text-base sm:text-lg font-bold leading-normal hover:text-primary line-clamp-3'>
+                          {post.title}
+                        </h2>
+                        <div className=''>
+                          <p className=' hidden sm:block text-sm text-gray-500 line-clamp-3'>
+                            {truncatedText(toPlainText(post.body), 150)} 
+                            <span className=' font-normal text-black/90 dark:text-white/80'>Read more</span>
+                          </p>
+                        </div>
+                        <p className=' text-xs text-gray-400'>
+                          {formatDistanceToNow(new Date(post.publishedAt), { addSuffix: true })}
+                        </p>
                       </div>
                     </div>
                   </Link>
@@ -149,3 +192,6 @@ const LatestPosts = () => {
 }
 
 export default LatestPosts
+
+
+//border border-gray-300 shadow-sm rounded-sm
