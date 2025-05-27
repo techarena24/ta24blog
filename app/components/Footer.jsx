bee996550bfcb2d1ad7d11900d8559b3f63ae650
Festus <techarenaz01@gmail.com>
2025-05-26 19:11:47 -0700

    Fixed the footer logo error and logo error message that appears on Dev Tool console.

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,56 +1,75 @@
-import { Facebook, Instagram, Twitter, X, Youtube } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { Facebook, Instagram, Twitter, X, Youtube } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
-
   return (
-    <footer className=' w-full max-w-6xl mx-auto bg-primary mt-18'>
-       <div className=' flex flex-col gap-10 sm:flex-row py-8 md:py-16 mx-6 lg:mx-20 justify-between sm:gap-0'>
-            <div className=' flex flex-col items-center sm:block sm:items-start'>
-               <Link href={"/"}>
-                    <Image 
-                         src={"/images/logoTA24.jpeg"}
-                         alt={"Logo"}
-                         width={1000}
-                         height={700}
-                         priority
-                         className=' mb-5 h-[75px] w-[150px]'
-                    />
-               </Link>
-               <p className=' text-xs font-medium mb-1'>
-                    Your Everyday Technology News
-               </p>
-               <div className=' flex gap-6'>
-                    <Twitter />
-                    <Instagram />
-                    <Facebook />
-                    <Youtube />
-               </div>
-            </div>
-            <div className=' flex flex-col space-y-6'>
-                <Link className=' font-medium text-sm' href={"/about"}>About</Link>
-                <Link className=' font-medium text-sm' href={"/news"}>News</Link>
-                <Link className=' font-medium text-sm' href={"/contact"}>Contact Us</Link>
-                <Link className=' font-medium text-sm' href={""}>Advertise</Link>
-                <Link className=' font-medium text-sm' href={""}>Privacy Policy</Link>
-            </div>
-            <div className=' flex flex-col space-y-6'>
-                <Link className=' font-medium text-sm' href={"/deals"}>Deals</Link>
-                <Link className=' font-medium text-sm' href={"/reviews"}>Reviews</Link>
-                <Link className=' font-medium text-sm' href={""}>Phone Comparison</Link>
-                <Link className=' font-medium text-sm' href={""}>Phone Specifications</Link>
-                <Link className=' font-medium text-sm' href={""}>Promotions</Link>
-            </div>
-       </div>
-       <div className=' flex justify-center bg-black py-2'>
-            <p className=' text-white text-xs'>
-                &copy; 2024 TechArena24. All rights reserved.
-            </p>
-       </div>
+    <footer className=" w-full max-w-6xl mx-auto bg-primary mt-18">
+      <div className=" flex flex-col gap-10 sm:flex-row py-8 md:py-16 mx-6 lg:mx-20 justify-between sm:gap-0">
+        <div className=" flex flex-col items-center sm:block sm:items-start">
+          <Link href={"/"}>
+            <Image
+              src={"/images/logoTA24.jpeg"}
+              alt={"Logo"}
+              width={1000}
+              height={700}
+              priority
+              className=" mb-5 h-[75px] w-[150px]"
+            />
+          </Link>
+          <p className=" text-xs font-medium mb-1">
+            Your Everyday Technology News
+          </p>
+          <div className=" flex gap-6">
+            <Twitter />
+            <Instagram />
+            <Facebook />
+            <Youtube />
+          </div>
+        </div>
+        <div className=" flex flex-col space-y-6">
+          <Link className=" font-medium text-sm" href={"/about"}>
+            About
+          </Link>
+          <Link className=" font-medium text-sm" href={"/news"}>
+            News
+          </Link>
+          <Link className=" font-medium text-sm" href={"/contact"}>
+            Contact Us
+          </Link>
+          <Link className=" font-medium text-sm" href={""}>
+            Advertise
+          </Link>
+          <Link className=" font-medium text-sm" href={""}>
+            Privacy Policy
+          </Link>
+        </div>
+        <div className=" flex flex-col space-y-6">
+          <Link className=" font-medium text-sm" href={"/deals"}>
+            Deals
+          </Link>
+          <Link className=" font-medium text-sm" href={"/reviews"}>
+            Reviews
+          </Link>
+          <Link className=" font-medium text-sm" href={""}>
+            Phone Comparison
+          </Link>
+          <Link className=" font-medium text-sm" href={""}>
+            Phone Specifications
+          </Link>
+          <Link className=" font-medium text-sm" href={""}>
+            Promotions
+          </Link>
+        </div>
+      </div>
+      <div className=" flex justify-center bg-black py-2">
+        <p className=" text-white text-xs">
+          &copy; 2024 TechArena24. All rights reserved.
+        </p>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
