6f945e60706bf3cdbd4d3f3f675b121d0ffc9b3b
Festus <techarenaz01@gmail.com>
2025-07-12 00:14:48 -0700

    added Ezoic js verification code and moved all js verification code to Clientscript.jsx

--- app/components/ClientScripts.jsx
+++ app/components/ClientScripts.jsx
@@ -13,10 +13,34 @@ export default function ClientScripts() {
       /> */}
 
       <Script
+        src="https://cmp.gatekeeperconsent.com/min.js"
+        data-cfasync="false"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://the.gatekeeperconsent.com/cmp.min.js"
+        data-cfasync="false"
+        strategy="afterInteractive"
+      />
+
+      <Script async src="//www.ezojs.com/ezoic/sa.min.js">
+        {`window.ezstandalone = window.ezstandalone || {}; ezstandalone.cmd =
+        ezstandalone.cmd || [];`}
+      </Script>
+
+      {/* google adsense code */}
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1557100683793492"
+        strategy="lazyOnload"
+        crossOrigin="anonymous"
+      ></Script>
+
+      {/* <Script
         id="cookie-script"
         strategy="afterInteractive"
         src="//cdn.cookie-script.com/s/9cc0a158b8f01befcf6531ca1f825fe3.js"
-      />
+      /> */}
 
       {/* <Script
         id="adsbygoogle"
